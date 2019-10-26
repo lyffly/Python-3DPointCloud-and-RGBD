6d6dd8807e1c99ab72aa89ec529d37f97d1e8778
liuyf <lyfflyl@gmail.com>
2019-10-26 17:32:25 +0800

    modify files

--- open3d/11_read_TUM_rgbd.py
+++ open3d/11_read_TUM_rgbd.py
@@ -12,6 +12,18 @@ filename_depth = 'demodata/RGBD/other_formats/TUM_depth.png'
 
 
 def plot_2_images(img1,img2,str1,str2):
+    """
+    plot 2 images in 1 window using matplotlib
+    
+    args:
+        img1: image1 numpy format
+        img2: image2 numpy format
+        str1:image 1 title
+        str2:image 2 title
+    
+    returns:
+        None
+    """
     plt.subplot(1,2,1)
     plt.title(str1)
     plt.imshow(img1)
@@ -23,30 +35,27 @@ def plot_2_images(img1,img2,str1,str2):
 
 if __name__ == "__main__":
     
-    #read sun datasets
+    # color 和depth 数据读取
     color_raw = op3.io.read_image(filename_color)
     depth_raw = op3.io.read_image(filename_depth)
-    
+    # 由color和depth数据生成rgbd 数据
     rgbd_image = op3.geometry.RGBDImage.create_from_tum_format(
         color_raw,depth_raw
     )
     print(rgbd_image)
 
 
-    plt.subplot(1,2,1)
-    plt.title("TUM grayscale image")
-    plt.imshow(rgbd_image.color)
-    plt.subplot(1,2,2)
-    plt.title("depth image")
-    plt.imshow(rgbd_image.depth)
-    plt.show()
-
+    # 显示在窗口中
+    plot_2_images(rgbd_image.color,rgbd_image.depth,
+                "TUM dataset color image","depth image")
+    # 由rgbd数据 生成 点云数据
     pcd = op3.geometry.PointCloud.create_from_rgbd_image(
         rgbd_image,
         op3.camera.PinholeCameraIntrinsic(op3.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault)
         )
     
     pcd.transform([[1,0,0,0],[0,-1,0,0],[0,0,-1,0],[0,0,0,1]])
+    # 点云数据显示
     op3.visualization.draw_geometries([pcd])
 
 
--- open3d/12_RGBD_odometry.py
+++ open3d/12_RGBD_odometry.py
@@ -8,30 +8,37 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
+    # 针孔相机的内参
     pinhole_camera_intrinsic =op3.io.read_pinhole_camera_intrinsic("demodata/camera_primesense.json")
     print(pinhole_camera_intrinsic.intrinsic_matrix)
 
-    #read sun datasets
+    # 读两幅图的 RGB和depth图
     source_color = op3.io.read_image('demodata/RGBD/color/00000.jpg')
     source_depth = op3.io.read_image('demodata/RGBD/depth/00000.png')
     target_color = op3.io.read_image('demodata/RGBD/color/00001.jpg')
     target_depth = op3.io.read_image('demodata/RGBD/depth/00001.png')
     
+    # source image 原始图
     source_rgbd_image = op3.geometry.RGBDImage.create_from_color_and_depth(
         source_color,source_depth
     )
+    # target image 目标图
     target_rgbd_image = op3.geometry.RGBDImage.create_from_color_and_depth(
         target_color,target_depth
     )
+
+    # 创建点云
     target_pcd = op3.geometry.PointCloud.create_from_rgbd_image(
         target_rgbd_image,
         pinhole_camera_intrinsic
         )
+    # 里程计参数选项
     option = op3.odometry.OdometryOption()
+    # 初始变换矩阵
     odo_init = np.identity(4)
     print(option)
 
-
+    #计算从source到target 的变换矩阵
     [success_color_term,trans_color_term,info] = op3.odometry.compute_rgbd_odometry(
         source_rgbd_image,
         target_rgbd_image,
@@ -41,13 +48,16 @@ if __name__ == "__main__":
         option
     )
 
+
     if success_color_term:
-        print("using RGB-D Odometry")
+        print("using RGB-D Odometry \n 使用RGBD里程计计算：")
         print(trans_color_term)
         source_pcd_color_term = op3.geometry.PointCloud.create_from_rgbd_image(
             source_rgbd_image,pinhole_camera_intrinsic
         )
+
         source_pcd_color_term.transform(trans_color_term)
+        
         op3.visualization.draw_geometries([target_pcd,source_pcd_color_term])
 
     
